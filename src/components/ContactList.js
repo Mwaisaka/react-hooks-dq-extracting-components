a7dbf468fdfc2dd1a2aea4360a7930709ed0ae82
Frank <frankkashere@gmail.com>
2023-12-27 15:41:51 +0300

    Complete code

---
+++ src/components/ContactList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ContactItem from "./ContactItem";
+
+function ContactList(props) {
+  const contactItems = props.contacts.map((contact) => {
+    return <ContactItem key={contact.id} name={contact.name} />;
+  });
+  return (
+    <>
+      <nav>
+        <h2>Contacts</h2>
+        <ul className="contacts">{contactItems}</ul>
+      </nav>
+    </>
+  );
+}
+
+export default ContactList;
